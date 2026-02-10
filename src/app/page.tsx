c1467490bb5d3c03fb560733c98ba6f7755f7852
Franco-JG <jeftefranco@outlook.com>
2026-02-09 19:05:53 -0600

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-6xl">
+        Welcome to <span className="text-green-400">Gas Tracker</span>
+      </h1>
+    </div>
+  );
+}
