db1551f6c7ae8a086c0bed15144bd2c86b7de283
Franco-JG <jeftefranco@outlook.com>
2026-02-18 16:22:32 -0600

    chore: card updated

--- src/components/dashboard/StationCard.tsx
+++ src/components/dashboard/StationCard.tsx
@@ -1,37 +1,50 @@
+import { LuBuilding2, LuHeart, LuMap } from "react-icons/lu"
+import { FaLocationArrow } from "react-icons/fa"
 import { StationWithDistance } from '@/types'
 
 
-export const StationCard = ({ creId, name, prices, distance, lat, lng }: StationWithDistance) => {
+export const StationCard = ({ name, prices, distance, lat, lng }: StationWithDistance) => {
 
   const routeUrl = "https://www.google.com/maps/dir/?api=1&destination="
 
   return (
-    <div className="bg-white p-4 rounded-xl shadow-sm border border-gray-100 hover:shadow-md transition-shadow">
-      <div className="flex justify-between items-start mb-3 gap-3">
+    <div className="bg-white p-4 space-y-4 rounded-xl shadow-sm border border-gray-100 hover:shadow-md transition-shadow">
+      {/* Header Card */}
+      <div className="flex justify-between items-start gap-3">
         {/* left side */}
         <div className='flex flex-1 min-w-0 items-center gap-3'>
           {/* Placeholder para el logo o imagen de la estación */}
-          <span className='shrink-0 w-12 h-12 bg-tertiary-6 animate-pulse transition-colors ease-in rounded-sm'></span>
+          <span className='shrink-0 w-12 h-12 bg-tertiary-6 ease-in rounded-sm flex items-center justify-center'>
+            <LuBuilding2 className="text-tertiary-1 text-lg" />
+          </span>
           {/* Nombre de la estación con efecto marquee*/}
-          <div className="min-w-0 overflow-hidden">
-            {/* ID CRE de la estación */}
-            <span className="text-xs font-bold text-gray-400 uppercase">{creId}</span>
+          <div className="min-w-0 space-y-1">
             {/* Nombre de la estación */}
-            <div className="relative overflow-hidden mask-fade">
+            <div className="overflow-hidden mask-fade">
               <div className="animate-marquee">
-                <h3 className="border-red-500 font-bold text-lg text-title leading-none whitespace-nowrap pr-8">
+                <h3 className="font-bold text-base text-title leading-none whitespace-nowrap pr-8">
                   {name}
                 </h3>
-                <h3 className="font-bold text-lg text-title leading-none whitespace-nowrap pr-8" aria-hidden="true">
+                <h3 className="font-bold text-base text-title leading-none whitespace-nowrap pr-8" aria-hidden="true">
                   {name}
                 </h3>
               </div>
             </div>
+            {/* ID CRE de la estación */}
+            {/* <span className="text-xs font-bold text-gray-400 uppercase">{creId}</span> */}
+            {distance && (
+              <div className="flex items-center gap-1">
+                <FaLocationArrow className="text-subtitle w-2.5" />
+                <span className="text-subtitle text-xs">{(distance / 1000).toFixed(1)} km</span>
+              </div>
+            )
+            }
+
           </div>
         </div>
         {/* right side */}
         <div className="text-left">
-          { 
+          {
             prices.find(p => p.type === 'regular') &&
             (
               <div className="text-2xl font-black text-primary-1">
@@ -43,28 +56,30 @@ export const StationCard = ({ creId, name, prices, distance, lat, lng }: Station
         </div>
       </div>
 
-      {/* Otros precios */}
-      <div className="flex gap-4 text-sm text-gray-600 mb-4 bg-gray-50 p-2 rounded-lg">
-        {prices.map(p => (
-          p.type !== 'regular' && (
-            <div key={p.id} className="flex gap-1">
-              <span className="capitalize font-medium">{p.type}:</span>
-              <span className="font-bold">${(p.price / 100).toFixed(2)}</span>
-            </div>
-          )
+      {/* Other prices */}
+      <div className="flex gap-3">
+        {prices.filter(p => p.type !== 'regular').map(p => (
+          <div key={p.id} className="flex basis-1/2 grow-0 text-gray-600 bg-gray p-2 rounded-lg justify-between items-center">
+            <span className="uppercase text-[10px] font-bold text-tertiary-4">{p.type}:</span>
+            <span className="font-bold text-sm text-title">${(p.price / 100).toFixed(2)}</span>
+          </div>
         ))}
       </div>
 
       {/* Footer Card */}
-      <div className="flex justify-between items-center border-t border-gray-100 pt-3 mt-2">
-        <span className="text-sm text-gray-500 flex items-center gap-1">
-          📍 {(distance / 1000).toFixed(1)} km
-        </span>
+      <div className="flex gap-2">
         <button
+          className="grow cursor-pointer bg-white border-2 border-primary-5 rounded-lg"
           onClick={() => window.open(`${routeUrl}${lat},${lng}`, '_blank')}
-          className="cursor-pointer px-4 py-1.5 bg-emerald-50 text-emerald-600 text-sm font-semibold rounded-full hover:bg-emerald-100 transition-colors">
-          Ver Mapa
+          >
+          <span className="flex gap-2 items-center justify-center">
+            <LuMap className="text-primary-1" strokeWidth={2} size={16} />
+            <span className="font-bold text-sm text-primary-1">Ver Mapa</span>
+          </span>
         </button>
+        <span className="bg-gray rounded-lg p-2">
+          <LuHeart size={24} className="text-primary-1"/>
+        </span>
       </div>
     </div>
   )
